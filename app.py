d8051886fdd49d61e6de31df17a8bf1b54cf76a3
Brijeshtanwar <tanwarbrijesh007@gmail.com>
2023-10-13 00:01:51 +0530

    working QA with private docs - check-1

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+# from langchain.chat_models import ChatOpenAI
+# from langchain.memory import ConversationBufferMemory
+# from langchain.chains import ConversationalRetrievalChain
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
 
 
 def get_pdf_text(pdf_docs):
@@ -11,7 +19,6 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
-
 
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
@@ -23,27 +30,57 @@ def get_text_chunks(text):
     chunks = text_splitter.split_text(text)
     return chunks
 
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+def get_conversation_chain(vectorstore):
+    llm=ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+
 def main():
+    load_dotenv()
     st.set_page_config(page_title='Chat with multi PDF', page_icon=":books")
+    st.header("Chat with multi PDFs :books:") 
+        
+    # Upload files
+    pdf_docs = st.file_uploader("upload your PDFs here and click on process", accept_multiple_files=True)
+    
+    if pdf_docs is not None:
+        # get pdf text
+        raw_text = get_pdf_text(pdf_docs)
 
-    st.header("Chat with multi PDFs :books:")
-    st.text_input("Ask any question from the PDFs")
+        # get text chunks
+        text_chunks = get_text_chunks(raw_text)
 
-    with st.sidebar:
-        st.subheader("your docs")
-        pdf_docs = st.file_uploader("upload your PDFs here and click on process", accept_multiple_files=True)
-        if st.button("process"):
-            with st.spinner("processing"):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
+        # create Vectorstore
+        vectorstore = get_vectorstore(text_chunks)
 
-                # get text chunks
-                text_chunks = get_text_chunks(raw_text)
-                st.write(text_chunks)
+        # create conversation chain
+        # st.session_state.conversation=get_conversation_chain(vectorstore)
 
+        # User question
+        user_question = st.text_input("Ask a question about your PDF: ")
+        if user_question:
+            docs = vectorstore.similarity_search(user_question)
 
-
-
+            llm=OpenAI()
+            chain = load_qa_chain(llm, chain_type='stuff')
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=user_question)
+                print(cb)
+            
+            st.write(response)
+            
+        
 
 if __name__ == '__main__':
     main()
